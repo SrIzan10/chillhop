5365ed71d05a52d0e254a069650cf6acb372df6e
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-04-18 14:53:38 +0200

    chore: use radio icon

--- src/lib/components/app/station-dropdown.svelte
+++ src/lib/components/app/station-dropdown.svelte
@@ -1,21 +1,24 @@
 <script lang="ts">
-  import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
-  import { buttonVariants } from "$lib/components/ui/button/index.js";
-  import { state as appState } from "@/state.svelte";
+  import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+  import { buttonVariants } from '$lib/components/ui/button/index.js';
+  import { state as appState } from '@/state.svelte';
+  import Radio from '@lucide/svelte/icons/radio';
 
   let selectedStationId = $state(appState.currentStation!.toString());
   $effect(() => {
     if (selectedStationId) {
       appState.currentStation = parseInt(selectedStationId);
     }
-  })
+  });
 </script>
-  
+
 <DropdownMenu.Root>
-  <DropdownMenu.Trigger class={buttonVariants({ variant: "default" })}
-   >Open</DropdownMenu.Trigger
+  <DropdownMenu.Trigger class={buttonVariants({ variant: 'default', size: 'icon' })}>
+    <Radio />
+  </DropdownMenu.Trigger>
+  <DropdownMenu.Content
+    class="w-56 bg-white/10 backdrop-blur-md transition hover:bg-white/20 text-foreground border border-white/20 shadow-lg"
   >
-  <DropdownMenu.Content class="w-56 bg-white/10 backdrop-blur-md transition hover:bg-white/20 text-foreground border border-white/20 shadow-lg">
     <DropdownMenu.Group>
       <DropdownMenu.GroupHeading>Select station</DropdownMenu.GroupHeading>
       <DropdownMenu.Separator />
