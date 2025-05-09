5046c2f7133a5523b22f3410d6f317d8a991896b
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2025-05-09 22:34:09 +0200

    feat: dialog is finally good enough i think

--- src/lib/components/ui/dialog/dialog-content.svelte
+++ src/lib/components/ui/dialog/dialog-content.svelte
@@ -25,17 +25,16 @@
     class={cn(
       'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:rounded-lg',
       'backdrop-blur-md border drop-shadow-md shadow-lg',
-      'filter backdrop-invert',
-      'bg-black/40 dark:bg-white/30 transition border-white/20 dark:border-black/20',
+      'dark:bg-black/20 bg-white/20 transition border-white/20 dark:border-black/20',
       className
     )}
     {...restProps}
   >
-    <div class="text-foreground max-w-none space-y-3">
+    <div class="text-foreground/90 max-w-none space-y-3 font-medium">
       {@render children?.()}
     </div>
     <DialogPrimitive.Close
-      class="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none backdrop-blur-sm p-1 shadow-sm bg-black/40 dark:bg-white/20 hover:bg-black/60 dark:hover:bg-white/40"
+      class="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none backdrop-blur-sm p-1 shadow-sm dark:bg-black/20 bg-white/20 transition border-white/20 dark:border-black/20"
     >
       <X class="size-4" color="white" />
       <span class="sr-only">Close</span>
